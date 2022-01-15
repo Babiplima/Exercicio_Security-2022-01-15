67186a1b79f112e6c34f89fd74c7f3839cbb672f
Bárbara Ann Lima <babiplima@gmail.com>
2022-01-15 09:47:26 -0300

    Criar configurações do Cors

--- src/main/java/br/com/zup/LeadCollector/config/Security/ConfiguracaoDeSeguranca.java
+++ src/main/java/br/com/zup/LeadCollector/config/Security/ConfiguracaoDeSeguranca.java
@@ -5,6 +5,9 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 @EnableWebSecurity
@@ -13,8 +16,15 @@ public class ConfiguracaoDeSeguranca extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable();
+        http.cors().configurationSource(configurationCors());
 
         http.authorizeRequests().antMatchers(HttpMethod.POST,"/leads").permitAll()
                 .anyRequest().authenticated();
+    }
+
+    CorsConfigurationSource configurationCors(){
+        UrlBasedCorsConfigurationSource cors = new UrlBasedCorsConfigurationSource();
+        cors.registerCorsConfiguration("/**",new CorsConfiguration().applyPermitDefaultValues());
+        return cors;
     }
 }
