9e7b3ac30aef0cccf4e3eab1ab33049e51c3d7a5
Bárbara Ann Lima <babiplima@gmail.com>
2022-01-15 09:34:10 -0300

    Manipular metodo configure HttpSecurity

--- src/main/java/br/com/zup/LeadCollector/config/Security/ConfiguracaoDeSeguranca.java
+++ src/main/java/br/com/zup/LeadCollector/config/Security/ConfiguracaoDeSeguranca.java
@@ -1,9 +1,20 @@
 package br.com.zup.LeadCollector.config.Security;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
 @EnableWebSecurity
-public class ConfiguracaoDeSeguranca {
+public class ConfiguracaoDeSeguranca extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable();
+
+        http.authorizeRequests().antMatchers(HttpMethod.POST,"/leads").permitAll()
+                .anyRequest().authenticated();
+    }
 }
